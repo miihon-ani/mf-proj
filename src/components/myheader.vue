74f8e8c1161110a0a4339acb741f9243651e20d2
miihon-ani <george@asatsuki.netgamers.jp>
2020-10-28 14:54:08 +0000

    0.1.2ver表記の更新漏れを反映

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Monster Farm 2 いろいろ計算機（にしたい）v0.1.1</p>
+    <p>Monster Farm 2 いろいろ計算機（にしたい）v0.1.2</p>
     <p>
       バグ報告など Twitter:
       <a href="https://twitter.com/miihon_ani">miihon_ani</a>
