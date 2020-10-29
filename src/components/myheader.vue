63812973a3a24fd1624d24974c8625bca070e17f
miihon-ani <george@asatsuki.netgamers.jp>
2020-10-29 02:33:51 +0000

    v0.2.0ビルド（数値変更漏れ＋データ調整分）

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <p>Monster Farm 2 いろいろ計算機（にしたい）v0.1.2</p>
+    <p>Monster Farm 2 いろいろ計算機（にしたい）v0.2.0</p>
     <p>
-      バグ報告など Twitter:
+      バグ報告などはTwitter:
       <a href="https://twitter.com/miihon_ani">miihon_ani</a>
+      まで
     </p>
   </div>
 </template>
